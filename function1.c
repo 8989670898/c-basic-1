5b6c6d0cb5e2ee2d58414743245a8944c88d931c
8989670898 <raiparth2@gmail.com>
2019-10-11 19:34:10 +0530

    Function coding starting

---
+++ function1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ main()
+{
+    printf("\n I am in main");
+    italy();
+    printf("\nI am finally in airplane");
+}
+italy()
+{
+    printf("\nI m finally italy");
+    brazil();
+    printf("\nI m back in italy");
+}
+brazil()
+{
+    printf("\nI am in brazil");
+}
